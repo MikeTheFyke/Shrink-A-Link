1c48151fc62c0cda6085fe02e93ec7a15e894081
MikeTheFyke <mikefyke@hotmail.com>
2024-02-09 21:53:25 -0500

    Built new register route and register form

--- express_server.js
+++ express_server.js
@@ -57,6 +57,14 @@ app.post("/logout", (req, res) => {
 	res.redirect("urls");
 });
 
+app.get("/register", (req, res) => {
+	const templateVars = {
+		username: req.cookies.username,
+		urls: urlDatabase,
+	};
+	res.render("register", templateVars);
+});
+
 app.get("/urls", (req, res) => {
 	const templateVars = {
 		username: req.cookies.username,
